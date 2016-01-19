14b80e293218a2dc663bee6c45a301266939d0e4
zhangyan8212 <zy@zydeMacBook-Pro.local>
2016-01-19 18:40:22 +0800

    第一天
    第一天

---
+++ tutorial/spiders/tutorial_spider.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2016年1月19日
+
+@author: zy
+'''
+import scrapy
+
+class tutorialSpider(scrapy.Spider):
+    name = "tutorial"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
