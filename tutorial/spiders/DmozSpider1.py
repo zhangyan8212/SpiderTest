cb6f86d51502f7977fa4e52856d5b1209af5acac
zhangyan8212 <zhangyan8212@163.com>
2016-01-20 19:07:53 +0800

    修改了一下

--- tutorial/spiders/DmozSpider1.py
+++ tutorial/spiders/DmozSpider1.py
@@ -5,23 +5,25 @@ Created on 2016年1月20日
 @author: zy
 '''
 import scrapy
+
 from tutorial.items import TutorialItem
-'''
-hahaha
-'''
-class DmozSpider(scrapy.Spider):
+
+class DmozSpider1(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
     ]
 
     def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
         for sel in response.xpath('//ul/li'):
             item = TutorialItem()
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
-            #print(title, link, desc)
             yield item
